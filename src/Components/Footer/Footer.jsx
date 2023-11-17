6c8f1fc98b029eb4991cf3fe6ddb06beab8cffcb
Yosef Alemu <yosefalemumengstie@gmail.com>
2023-11-17 10:44:55 +0300

    intial setup with nextjs done

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <div>Left Footer</div>
+      <div>Right Footer</div>
+    </div>
+  );
+};
+
+export default Footer;
